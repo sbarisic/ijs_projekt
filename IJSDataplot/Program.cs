cd4f75557ed9a9909a0a23e03d8d19fddbe575ea
Saša Barišić <cartman300@net.hr>
2018-05-03 13:12:39 +0200

    C#

---
+++ IJSDataplot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using System.Reflection;
+
+namespace IJSDataplot {
+	class Program {
+		static void Main(string[] args) {
+			CSharpCodeProvider Provider = new CSharpCodeProvider();
+			CompilerParameters Params = new CompilerParameters();
+			Params.ReferencedAssemblies.Add("IJSDataplot.exe");
+			Params.GenerateInMemory = true;
+			Params.GenerateExecutable = false;
+
+			string[] CSFiles = Directory.GetFiles("source", "*.cs");
+			CompilerResults Res = Provider.CompileAssemblyFromFile(Params, CSFiles);
+
+			if (Res.Errors.HasErrors) {
+				foreach (CompilerError E in Res.Errors)
+					Console.WriteLine("Error ({0}): {1}", E.ErrorNumber, E.ErrorText);
+
+			} else {
+				Assembly A = Res.CompiledAssembly;
+				MethodInfo MainMethod;
+
+				foreach (var T in A.ExportedTypes)
+					if ((MainMethod = T.GetMethod("Main")) != null)
+						MainMethod.Invoke(null, null);
+			}
+
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ IJSDataplot/source/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IJSDataplot {
+	public static class Program {
+		public static void Main() {
+			Console.WriteLine("Test!");
+		}
+	}
+}
