c8421906ad61bd27625f5d6d6d2f7373e43407c6
Saša Barišić <cartman300@net.hr>
2018-05-10 00:28:58 +0200

    Using FishGf

--- IJSDataplot/Program.cs
+++ IJSDataplot/Program.cs
@@ -3,112 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.CodeDom.Compiler;
-using Microsoft.CSharp;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using Glfw3;
-using OpenGL;
-using System.Diagnostics;
+using System.Numerics;
+
+using FishGfx;
+using FishGfx.Graphics;
+using FishGfx.System;
 
 namespace IJSDataplot {
 	class Program {
-		static Glfw.Window Window;
-
-		static Glfw.Monitor GetDesktopResolution(out int Width, out int Height) {
-			Glfw.Monitor Monitor = Glfw.GetPrimaryMonitor();
-
-			Glfw.VideoMode VideoMode = Glfw.GetVideoMode(Monitor);
-			Width = VideoMode.Width;
-			Height = VideoMode.Height;
-
-			return Monitor;
-		}
-
-		static Glfw.Window CenterWindow(Glfw.Window Wnd) {
-			GetDesktopResolution(out int W, out int H);
-			Glfw.GetWindowSize(Wnd, out int WW, out int WH);
-
-			int X = W / 2 - WW / 2;
-			int Y = H / 2 - WH / 2;
-
-			Glfw.SetWindowPos(Wnd, X, Y);
-			return Wnd;
-		}
+		static RenderWindow RWind;
 
 		static void Main(string[] args) {
-			Glfw.ConfigureNativesDirectory("dlls/native/glfw3_64");
-			if (!Glfw.Init())
-				throw new Exception("Could not initialize GLFW");
+			const float Scale = 0.9f;
 
-			Glfw.WindowHint(Glfw.Hint.Resizable, true);
+			RenderAPI.GetDesktopResolution(out int W, out int H);
+			RWind = new RenderWindow((int)(W * Scale), (int)(H * Scale), "Vector PFM");
 
-			//Glfw.WindowHint(Glfw.Hint.ClientApi, Glfw.ClientApi.None);
-			Glfw.WindowHint(Glfw.Hint.ClientApi, Glfw.ClientApi.OpenGL);
-			Glfw.WindowHint(Glfw.Hint.ContextCreationApi, Glfw.ContextApi.Native);
-			Glfw.WindowHint(Glfw.Hint.Doublebuffer, true);
-			Glfw.WindowHint(Glfw.Hint.ContextVersionMajor, 3);
-			Glfw.WindowHint(Glfw.Hint.ContextVersionMinor, 2);
+			ShaderProgram Default = new ShaderProgram(new ShaderStage(ShaderType.VertexShader, "data/default.vert"),
+				new ShaderStage(ShaderType.FragmentShader, "data/default.frag"));
 
-			//Glfw.WindowHint(Glfw.Hint.Samples, 2);
-			Glfw.WindowHint(Glfw.Hint.OpenglForwardCompat, true);
-			Glfw.WindowHint(Glfw.Hint.OpenglProfile, Glfw.OpenGLProfile.Core);
-#if DEBUG
-			Glfw.WindowHint(Glfw.Hint.OpenglDebugContext, true);
-#endif
+			Vector2 Viewport = RWind.GetWindowSizeVec();
+			Default.Uniforms.Viewport = Viewport;
+			Default.Uniforms.Project = Matrix4x4.CreateOrthographicOffCenter(0, Viewport.X, 0, Viewport.Y, -10, 10);
 
-			const float WindowSizeScale = 0.9f;
 
-			GetDesktopResolution(out int W, out int H);
-			Window = CenterWindow(Glfw.CreateWindow((int)(W * WindowSizeScale), (int)(H * WindowSizeScale), nameof(IJSDataplot)));
+			while (!RWind.ShouldClose) {
+				Gfx.Clear();
+				
 
-			Gl.Initialize();
-			Glfw.MakeContextCurrent(Window);
-			Init();
-
-			Stopwatch SWatch = Stopwatch.StartNew();
-			float Dt = 0;
-
-			while (!Glfw.WindowShouldClose(Window)) {
-				Glfw.PollEvents();
-				Update(Dt);
-
-				while (SWatch.ElapsedMilliseconds / 1000.0f < (1.0f / 60.0f))
-					;
-				Dt = SWatch.ElapsedMilliseconds / 1000.0f;
-				SWatch.Restart();
+				RWind.SwapBuffers();
+				Events.Poll();
 			}
-
-			Environment.Exit(0);
-		}
-
-		static void Init() {
-#if DEBUG
-			try {
-				Gl.DebugMessageCallback((Src, DbgType, ID, Severity, Len, Buffer, UserPtr) => {
-					Utils.Log(Marshal.PtrToStringAnsi(Buffer, Len));
-
-					if (Severity == Gl.DebugSeverity.Notification)
-						return;
-
-					if ((/*Severity == Gl.DebugSeverity.Medium ||*/ Severity == Gl.DebugSeverity.High) && Debugger.IsAttached)
-						Debugger.Break();
-				}, IntPtr.Zero);
-			} catch (Exception) {
-
-			}
-#endif
-		}
-
-		static void Update(float Dt) {
-			if (Dt == 0)
-				return;
-
-		}
-
-		static void Draw() {
-
 		}
 	}
 }
